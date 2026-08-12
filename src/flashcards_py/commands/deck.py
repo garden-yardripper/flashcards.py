cfcb54f00f75ba93977406ea9795d2fdb1961986
garden-yardripper <gardenyardripper@gmail.com>
2026-08-12 13:53:26 -0400

    Create deck command module with basic function stubs

---
+++ src/flashcards_py/commands/deck.py
@@ -0,0 +1,26 @@
+def do_create():
+    pass
+
+
+def do_edit():
+    pass
+
+
+def do_delete():
+    pass
+
+
+def do_select():
+    pass
+
+
+def do_list():
+    pass
+
+
+def do_import():
+    pass
+
+
+def do_export():
+    pass
